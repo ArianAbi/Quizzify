b8857b8a73eda9d5637b3e3239d2c7293b52c2de
Arian <kapqworks@gmail.com>
2024-03-31 16:36:12 +0330

    feat : get spotify access token (credentials)

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { getCredentials } from "./useSpotify";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function storeCredentials() {
+      try {
+        const credentials = await getCredentials();
+        localStorage.setItem("access-token", JSON.stringify(credentials));
+
+        setLoading(false);
+      } catch (err) {
+        console.error("SOME ERROR OCCURRED : " + err);
+      }
+    }
+
+    storeCredentials();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <span>Hi , This is a Test</span>
+        <span>{loading ? "loading..." : "All Set"}</span>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
