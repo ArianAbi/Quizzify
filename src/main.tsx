e01cacfe7dce12bae08c2c1f50f10435d4083fb9
Arian <kapqworks@gmail.com>
2024-04-05 23:55:51 +0330

    add : tailwind css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./css/index.css";
+import "./css/global.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Game from "./Game.tsx";
 
