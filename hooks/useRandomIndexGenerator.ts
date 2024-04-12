1e96189f76d2c6e94a5976954d7f9295cfacb510
Arian <kapqworks@gmail.com>
2024-04-12 18:47:33 +0330

    fix : generate random numbers that are actually in range

--- hooks/useRandomIndexGenerator.ts
+++ hooks/useRandomIndexGenerator.ts
@@ -2,7 +2,7 @@ export function generateRandomIndexInRange(max: number) {
   let indexes: number[] = [];
 
   while (indexes.length < max) {
-    let r = Math.floor(Math.random() * max + 1);
+    let r = Math.floor(Math.random() * max);
     if (indexes.indexOf(r) === -1) indexes.push(r);
   }
 
