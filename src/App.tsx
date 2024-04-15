4b9e84a95b3136e32c1bd806b7b1d6e0cea96baa
Arian <kapqworks@gmail.com>
2024-04-15 15:16:45 +0330

    fix : store the spotify credentials at the root level

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
 import "./css/App.css";
 import { Link } from "react-router-dom";
+import { spotify_storeCredentials } from "../hooks/useSpotify";
 
 function App() {
+  useEffect(() => {
+    (async () => {
+      await spotify_storeCredentials();
+    })();
+  }, []);
+
   return (
     <>
       <div className="container">
